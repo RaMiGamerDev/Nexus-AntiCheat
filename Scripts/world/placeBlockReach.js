958811662a47b5d1254e971d7bbb124de7997c19
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2026-02-22 18:25:20 +0100

    Add files via upload

--- Scripts/world/placeBlockReach.js
+++ Scripts/world/placeBlockReach.js
@@ -2,7 +2,7 @@ import { world } from "@minecraft/server";
 import { getPing, detect, blockDistance, isOp } from "../utility/utility.js";
 import { v } from "../main.js";
 import { config } from "../config.js";
-world.beforeEvents.playerBreakBlock.subscribe((ev) => {
+world.beforeEvents.playerPlaceBlock.subscribe((ev) => {
     const reachCfg = config.modules.world.blockReach;
     const { block, player } = ev;
     if (!reachCfg.enabled || isOp(player) || !v()) return;
@@ -10,8 +10,7 @@ world.beforeEvents.playerBreakBlock.subscribe((ev) => {
     if (distance > reachCfg.max) {
         ev.cancel = true;
         const ping = getPing(player.id);
-        if (ping <= reachCfg.maxPing) {
-        detect({ flagMessage: `§f${player.name} §7failed §cBlock Reach §8§l(§r§cBreak§l§8) §8§l(§r§c${distance.toFixed(2)}§8§l/§r§a${reachCfg.max}§l§8) §8§l(§r§fPing§8: §7${ping.toFixed(2)} ms§8§l)`, staffTag: config.staff.utility.notifyTag });
-        }
+        if (ping >= reachCfg.maxPing) return;
+        detect({ flagMessage: `§f${player.name} §7failed §cBlock Reach §8§l(§r§cPlace§l§8) §8§l(§r§c${distance.toFixed(2)}§8§l/§r§a${reachCfg.max}§l§8) §8§l(§r§fPing§8: §7${ping.toFixed(2)} ms§8§l)`, staffTag: config.staff.utility.notifyTag });
     }
 });
